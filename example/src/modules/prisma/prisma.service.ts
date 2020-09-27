bc13cd016acf4ec6419a91f36c6e91e50d099c52
Jam Risser <jam@codejam.ninja>
2020-09-27 01:21:21 +0000

    Finished pnpm support

--- example/src/modules/prisma/prisma.service.ts
+++ example/src/modules/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma-client';
+import { PrismaClient } from '@prisma/generated';
 
 @Injectable()
 export class PrismaService
