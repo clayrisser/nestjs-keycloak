5b36b00ca42399644ae5c0b7f04509c44d4c8142
Clay Risser <email@clayrisser.com>
2021-07-14 11:47:20 -0500

    Started license headers

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,3 +1,27 @@
+/**
+ * File: /src/guards/auth.guard.ts
+ * Project: whisker-keycloak
+ * File Created: 14-07-2021 11:43:59
+ * Author: Clay Risser <email@clayrisser.com>
+ * -----
+ * Last Modified: 14-07-2021 11:45:55
+ * Modified By: Clay Risser <email@clayrisser.com>
+ * -----
+ * Silicon Hills LLC (c) Copyright 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Grant, Keycloak, Token } from 'keycloak-connect';
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
