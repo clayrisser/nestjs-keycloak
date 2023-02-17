2ac26be6ac27df9ef5462f0ee28eeec534cafd74
Manikanta <vrmk1995@gmail.com>
2023-02-17 10:46:27 +0530

    working on nodes and edges

--- src/index.ts
+++ src/index.ts
@@ -1,113 +1,71 @@
 import { ApacheAgeClient } from "apache-age-client";
 
-let client: any;
-async function ConnectToDatabase(
-  databaseName: string,
-  databaseHost: string,
-  databasePort: number,
-  databaseUser: string,
-  databasePassword: string,
-  graphName: string
-) {
-  client = await ApacheAgeClient.connect({
-    database: databaseName,
-    host: databaseHost,
-    password: databasePassword,
-    port: databasePort,
-    user: databaseUser,
-    graph: graphName,
-  });
-  console.log("Connected to database", client);
-}
-
-const createGraph = async (graphName: string) => {
-  return await client.executeCypher(`
-    SELECT * FROM ag_catalog.create_graph('${graphName}');
-  `);
-};
-
-const createNode = async (graphName: string, node: any) => {
-  return await client.executeCypher(`
-    SELECT * FROM ag_catalog.cypher('${graphName}', $$
-      CREATE (n:${node.label} {${node.properties}})
-    $$) as (v agtype);
-  `);
-};
-
-const createRelationship = async (
-  graphName: string,
-  relationship: any,
-  fromNode: any,
-  toNode: any
-) => {
-  return await client.executeCypher(`
- SELECT * FROM ag_catalog.cypher('${graphName}', $$
-      MATCH (a:${fromNode.label} {${fromNode.properties}}), (b:${toNode.label} {${toNode.properties}})
-      CREATE (a)-[e:${relationship.label}]->(b)
-      RETURN a, e, b
-    $$) as (a agtype, e agtype, b agtype);
-  `);
-};
-
 (async () => {
-  ConnectToDatabase(
-    "postgres",
-    "localhost",
-    5432,
-    "postgres",
-    "postgres",
-    "some-graph"
+  const client = await ApacheAgeClient.connect({
+    database: "postgres",
+    host: "localhost",
+    password: "postgres",
+    port: 5432,
+    user: "postgres",
+    graph: "some-graph",
+  });
+
+  console.log(
+    await client.executeCypher(`
+  SELECT * FROM ag_catalog.create_graph('some-graph');
+`)
   );
+
+  console.log(
+    await client.executeCypher(`
+  SELECT * FROM ag_catalog.cypher('some-graph', $$
+    CREATE (n:Person {name: 'Bob', age: 22, favorite_color: 'blue'})
+  $$) as (v agtype);
+`)
+  );
+
+  console.log(
+    await client.executeCypher(`
+  SELECT * FROM ag_catalog.cypher('some-graph', $$
+    CREATE (n:Dog {name: 'Fido', age: 12, favorite_color: 'green'})
+  $$) as (v agtype);
+`)
+  );
+
+  console.log(
+    await client.executeCypher(`
+  SELECT * FROM ag_catalog.cypher('some-graph', $$
+    CREATE (n:User {userId: 'mani', qualified: pass ,color:'red'})
+  $$) as (v agtype);
+`)
+  );
+
+  const referringUserId = "Bob";
+  const referredUserId = "Fido";
+  const referralId = "mani";
+
+  console.log(
+    await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('some-graph', $$
+      MATCH (a:User {userId: '${referredUserId}', qualified: false}), (b:User {userId: '${referringUserId}', qualified: false}',(c:User {userId: '${referralId}', qualified: false})}'))})
+      CREATE (a)-[e:REFERRAL]->(b)-[f:LOVER]->(C)
+      RETURN a, e, b,f,c
+    $$) as (a agtype, e agtype, b agtype f agtype c agtype);
+  `)
+  );
+
+  const userId = "Bob";
+  const qualified = true;
+
+  console.log(
+    await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('some-graph', $$
+      MATCH (a:User {userId: '${userId}'})
+      UPDATE a SET a.qualified = ${qualified}
+      RETURN a
+    $$) as (a agtype);
+  `)
+  );
+
+  await client.disconnect();
 })();
-
-// (async () => {
-// console.log(
-//   await client.executeCypher(`
-//   SELECT * FROM ag_catalog.create_graph('some-graph');
-// `)
-// );
-
-// console.log(
-//   await client.executeCypher(`
-//   SELECT * FROM ag_catalog.cypher('some-graph', $$
-//     CREATE (n:Person {name: 'Bob', age: 22, favorite_color: 'blue'})
-//   $$) as (v agtype);
-// `)
-// );
-
-// console.log(
-//   await client.executeCypher(`
-//   SELECT * FROM ag_catalog.cypher('some-graph', $$
-//     CREATE (n:Dog {name: 'Fido', age: 12, favorite_color: 'green'})
-//   $$) as (v agtype);
-// `)
-// );
-
-//   const referringUserId = "Bob";
-//   const referredUserId = "Fido";
-
-//   console.log(
-//     await client.executeCypher(`
-//     SELECT * FROM ag_catalog.cypher('some-graph', $$
-//       MATCH (a:User {userId: '${referredUserId}', qualified: false}), (b:User {userId: '${referringUserId}', qualified: false}'})
-//       CREATE (a)-[e:REFERRAL]->(b)
-//       RETURN a, e, b
-//     $$) as (a agtype, e agtype, b agtype);
-//   `)
-//   );
-
-//   const userId = "Bob";
-//   const qualified = true;
-
-//   console.log(
-//     await client.executeCypher(`
-//     SELECT * FROM ag_catalog.cypher('some-graph', $$
-//       MATCH (a:User {userId: '${userId}'})
-//       UPDATE a SET a.qualified = ${qualified}
-//       RETURN a
-//     $$) as (a agtype);
-//   `)
-//   );
-
-//   await client.disconnect();
-// })();
