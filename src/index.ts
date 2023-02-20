f0fea0af9da2b66743c85ca0d75adc5d6d80f852
Clay Risser <clayrisser@gmail.com>
2023-02-20 12:02:18 +0530

    add all the function related to id

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function ConnectToDatabase(
   databasePassword: string,
   graphName: string
 ) {
-  const client = await ApacheAgeClient.connect({
+  client = await ApacheAgeClient.connect({
     database: databaseName,
     host: databaseHost,
     password: databasePassword,
@@ -162,7 +162,7 @@ const deleteAllEdge = async (graphName: string) => {
   `);
 };
 
-const findById = async (graphName: string, node: any) => {
+const findVertexById = async (graphName: string, node: any) => {
   return await client.executeCypher(`
     SELECT * FROM ag_catalog.cypher('${graphName}', $$
       MATCH (n)
@@ -172,87 +172,230 @@ const findById = async (graphName: string, node: any) => {
   `);
 };
 
+const updateVertexById = async (graphName: string, node: any) => {
+  return await client.executeCypher(`
+     SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH (n)
+              where id=${node.id}
+      SET n.${node.name} = ${node.value}
+      RETURN n
+    $$) as (v agtype)
+  `);
+};
+
+const deleteVertexById = async (graphName: string, node: any) => {
+  return await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH (n)
+              where id=${node.id}
+      DETACH DELETE n
+    $$) as (v agtype);
+  `);
+};
+
+const findAllVertex = async (graphName: string) => {
+  return await client.executeCypher(` 
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH (n)
+      RETURN n
+    $$) as (v agtype);
+  `);
+};
+
+const findAllEdges = async (graphName: string) => {
+  return await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH ()-[e]-()
+      RETURN e
+    $$) as (v agtype);
+  `);
+};
+
+const findByVertexProperty = async (graphName: string, node: any) => {
+  return await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH (n)
+              where n.${node.name} = ${node.value}
+      RETURN n
+    $$) as (v agtype);
+  `);
+};
+
+const findByEdgeProperty = async (graphName: string, edge: any) => {
+  return await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH ()-[e]-()
+            where e.${edge.name} = ${edge.value}
+      RETURN e
+    $$) as (v agtype);
+  `);
+};
+
+const findEdgeById = async (graphName: string, edge: any) => {
+  return await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH ()-[e]-()
+        with e where id=${edge.id}
+      RETURN e
+    $$) as (v agtype);
+  `);
+};
+
+const updateEdgeById = async (graphName: string, edge: any) => {
+  return await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH ()-[e]-()
+        with e where id=${edge.id}
+      SET e.${edge.name} = ${edge.value}
+      RETURN e
+    $$) as (v agtype);
+  `);
+};
+
+const deleteEdgeById = async (graphName: string, edge: any) => {
+  return await client.executeCypher(`
+    SELECT * FROM ag_catalog.cypher('${graphName}', $$
+      MATCH ()-[e]-()
+        with e where id=${edge.id}
+      DELETE e
+    $$) as (v agtype);
+  `);
+};
+
 (async () => {
   await connection();
 
-  const create_graph = await createGraph("demo-graph");
-  console.log(create_graph);
+  // const create_graph = await createGraph("demo-graph");
+  // console.log(create_graph);
 
-  const drop_graph = await dropGraph("demo-graph");
-  console.log(drop_graph);
+  // const drop_graph = await dropGraph("demo-graph");
+  // console.log(drop_graph);
 
-  const deleteAllNodesInGraph = await deleteAllNodes("data-graph");
-  console.log(deleteAllNodesInGraph);
+  // const deleteAllNodesInGraph = await deleteAllNodes("data-graph");
+  // console.log(deleteAllNodesInGraph);
 
-  const create_node = await createNode("data-graph", {
-    label: "User",
-    properties: "userId: 'krishna', qualified: false",
-  });
-  console.log(create_node);
+  // const create_node = await createNode("data-graph", {
+  //   label: "User",
+  //   properties: "userId: 'krishna', qualified: false",
+  // });
+  // console.log(create_node);
 
-  const create_node_relationship = await matchNodeAndCreateRelationship(
-    "data-graph",
-    { label: "REFERRAL", properties: "referralCode: '1234'" },
-    { label: "User", properties: "userId: 'Mohan',age:25, qualified: false" },
-    { label: "User", properties: "userId: 'Rohan',age:24, qualified: false" }
-  );
-  console.log(create_node_relationship);
+  // const create_node_relationship = await matchNodeAndCreateRelationship(
+  //   "data-graph",
+  //   { label: "REFERRAL", properties: "referralCode: '1234'" },
+  //   { label: "User", properties: "userId: 'Mohan',age:25, qualified: false" },
+  //   { label: "User", properties: "userId: 'Rohan',age:24, qualified: false" }
+  // );
+  // console.log(create_node_relationship);
 
-  const create_nodes_and_relationship = await createNodesAndRelationship(
-    "data-graph",
-    { label: "REFERRAL", properties: "referralCode: '1236'" },
-    { label: "User", properties: "userId: 'Mohan', age:25,qualified: false" },
-    { label: "User", properties: "userId: 'Rohan',age:24, qualified: false" }
-  );
-  console.log(create_nodes_and_relationship);
+  // const create_nodes_and_relationship = await createNodesAndRelationship(
+  //   "data-graph",
+  //   {
+  //     label: "REFERRAL",
+  //     properties: "referralCode: '1285',label:'First Label'",
+  //   },
+  //   {
+  //     label: "User",
+  //     properties: "userId: 'Ram', age:23,qualified: false",
+  //   },
+  //   { label: "User", properties: "userId: 'Krishna',age:22, qualified: false" }
+  // );
+  // console.log(create_nodes_and_relationship);
 
-  const delete_node = await deleteNode("data-graph", {
-    label: "User",
-    properties: "userId: 'Rohan',age:24, qualified: false",
-  });
-  console.log(delete_node);
+  // const delete_node = await deleteNode("data-graph", {
+  //   label: "User",
+  //   properties: "userId: 'Rohan',age:24, qualified: false",
+  // });
+  // console.log(delete_node);
 
-  const update_node_property = await UpdateNodeProperty(
-    "data-graph",
-    { label: "User", properties: "userId: 'Mohan',age:28, qualified: false" },
-    { name: "userId", value: "'Sohan'" }
-  );
-  console.log(update_node_property);
+  // const update_node_property = await UpdateNodeProperty(
+  //   "data-graph",
+  //   { label: "User", properties: "userId: 'Mohan',age:28, qualified: false" },
+  //   { name: "userId", value: "'Sohan'" }
+  // );
+  // console.log(update_node_property);
 
-  const update_edge_property = await updateEdgeProperty(
-    "data-graph",
-    { label: "REFERRAL", properties: "referralCode: '1234'" },
-    { name: "referralCode", value: "'11122233'" }
-  );
-  console.log(update_edge_property);
+  // const update_edge_property = await updateEdgeProperty(
+  //   "data-graph",
+  //   { label: "REFERRAL", properties: "referralCode: '1234'" },
+  //   { name: "referralCode", value: "'11122233'" }
+  // );
+  // console.log(update_edge_property);
 
-  const delete_particular_edge = await deleteParticularRelationEdge(
-    "data-graph",
-    {
-      label: "REFERRAL",
-      properties: "referralCode: '1235'",
-    }
-  );
-  console.log(delete_particular_edge);
+  // const delete_particular_edge = await deleteParticularRelationEdge(
+  //   "data-graph",
+  //   {
+  //     label: "REFERRAL",
+  //     properties: "referralCode: '1235'",
+  //   }
+  // );
+  // console.log(delete_particular_edge);
 
-  const delete_all_edge = await deleteAllEdge("data-graph");
-  console.log(delete_all_edge);
+  // const delete_all_edge = await deleteAllEdge("data-graph");
+  // console.log(delete_all_edge);
 
-  const find_by_node_property = await findByNodeProperty("data-graph", {
-    label: "User",
-    properties: "userId: 'Mohan'",
-  });
-  console.log(find_by_node_property);
+  // const find_by_node_property = await findByNodeProperty("data-graph", {
+  //   label: "User",
+  //   properties: "userId: 'Mohan'",
+  // });
+  // console.log(find_by_node_property);
 
-  const find_by_label = await findByNodeLabel("data-graph", {
-    label: "Person",
-  });
-  console.log(find_by_label);
+  // const find_by_label = await findByNodeLabel("data-graph", {
+  //   label: "Person",
+  // });
+  // console.log(find_by_label);
 
-  const find_by_id = await findById("data-graph", {
-    id: 844424930131974,
-  });
-  console.log(find_by_id);
+  // const find_by_id = await findVertexById("data-graph", {
+  //   id: 844424930131970,
+  // });
+  // console.log(find_by_id);
+
+  // const update_by_id = await updateVertexById("data-graph", {
+  //   id: 844424930131970,
+  //   name: "userId",
+  //   value: "'Ram'",
+  // });
+  // console.log(update_by_id);
+
+  // const delete_by_id = await deleteVertexById("data-graph", {
+  //   id: 844424930131986,
+  // });
+  // console.log(delete_by_id);
+
+  // const find_all_vertex = await findAllVertex("data-graph");
+  // console.log(find_all_vertex);
+
+  // const find_all_edges = await findAllEdges("data-graph");
+  // console.log(find_all_edges);
+
+  // const find_by_node_property = await findByVertexProperty("data-graph", {
+  //   name: "qualified",
+  //   value: false,
+  // });
+  // console.log(find_by_node_property);
+
+  // const find_by_edge_property = await findByEdgeProperty("data-graph", {
+  //   name: "referralCode",
+  //   value: "'1285'",
+  // });
+  // console.log(find_by_edge_property);
+
+  // const find_edge_by_id = await findEdgeById("data-graph", {
+  //   id: 1125899906842632,
+  // });
+  // console.log(find_edge_by_id);
+
+  // const update_edge_by_id = await updateEdgeById("data-graph", {
+  //   id: 1125899906842632,
+  //   name: "referralCode",
+  //   value: "'1255'",
+  // });
+  // console.log(update_edge_by_id);
+
+  // const delete_edge_by_id = await deleteEdgeById("data-graph", {
+  //   id: 1125899906842632,
+  // });
+  // console.log(delete_edge_by_id);
 
   await client.disconnect();
 })();
